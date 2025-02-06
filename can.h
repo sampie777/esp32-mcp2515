edc1972dfb0976b989bdb6c5e12feef2d9f4c502
S. Jansen <samuelanton@hotmail.com>
2025-02-06 20:38:19 +0200

    refactor: Reformatted all code

--- can.h
+++ can.h
@@ -38,8 +38,8 @@ typedef __u32 canid_t;
 
 struct can_frame {
     canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-    __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
-    __u8    data[CAN_MAX_DLEN] __attribute__((aligned(8)));
+    __u8 can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+    __u8 data[CAN_MAX_DLEN] __attribute__((aligned(8)));
 };
 
 #endif /* CAN_H_ */
